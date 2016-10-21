15318b2d4e977d5bb009ac6eeee4a9fd929dfb55
xikang <zhangxk2009@gmail.com>
2016-10-20 21:13:16 -0400

    add shell folder

---
+++ shell/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# input file
+_db="./input.txt"
+
+# output file
+o="./output.txt"
+
+if [[ -f "$_db" ]]
+then
+
+while IFS= read -r line
+    do
+#echo "$line"
+    ffmpeg -i $line -vcodec copy -f rawvideo -y /dev/null 2>&1 | tr ^M '\n' | awk '/^frame=/ {print $2}'|tail -n 1
+done < $_db
+fi
